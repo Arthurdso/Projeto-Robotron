5622eab6862a0ea917bbe834a9565d93c90820c9
Arthur Oliveira <arthurdso96@gmail.com>
2022-08-02 20:50:45 -0300

    melhorando Robo
    adicionando função para acrescentar ou diminuir atributos.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+const robotron = document.getElementById('#robotron');
+
+const controle = document.querySelectorAll('.controle-ajuste')
+
+controle.forEach((elemento) => {
+    elemento.addEventListener('click', (e) => {
+        manipulaDados(e.target.textContent);
+    });
+});
+
+function manipulaDados(operacao) {
+    if(operacao === "-") {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
+}
